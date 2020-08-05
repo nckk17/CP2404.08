bb4063f93ea3cc96fc771b0f9d4cd30ef7afbf41
DELL <naychikoko1997@gmail.com>
2020-08-05 12:23:32 +0800

    prac1 loop

---
+++ prac1/loop.py
@@ -0,0 +1,24 @@
+
+def main():
+    for i in range(20,0,-1):
+        print(i, end=' ')
+        print()
+main()
+
+def main():
+    for i in range(0,100,10):
+        print(i, end=' ')
+        print()
+main()
+
+def main():
+ numberofLine=4
+ for i in range (1,numberofLine+1):
+        print("*",end="")
+main()
+
+def main():
+ numberofLine=4
+ for i in range(1,numberofLine+1):
+     print("*"*i)
+main()
